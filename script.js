f978c5379a76f96bb20a95a3af9218334f7bcaea
Shiven <Shiven905@gmail.com>
2022-08-26 14:16:23 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const inputButton = document.querySelector()
+
+//Calculator Class & constructor, setup 
+class Calculator {
+    
+    constructor() {
+
+    };
+    //this.inputButton = inputButton;
+};
